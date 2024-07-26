5f82e0ab3fcaa5b60c2e43c362bfd6d0693a7424
Ilya <105770380+Artivus2@users.noreply.github.com>
2024-07-26 10:27:01 +0700

    Add files via upload

--- App.vue
+++ App.vue
@@ -1,50 +1,50 @@
 <template>
-    <div>
+  <div>
+
+   <!-- <QRCodeVue3
+          value="Simple QR code"
+        /> -->
+
+   <QRCodeVue3
+          :width="500"
+          :height="500"
+          value="https://836b-213-234-222-188.ngrok-free.app/site/?otdel=2"
+          :qrOptions="{ typeNumber: 0, mode: 'Byte', errorCorrectionLevel: 'Q' }"
+          :imageOptions="{ hideBackgroundDots: true, imageSize: 0.4, margin: 0 }"
+          :dotsOptions="{
+          type: 'square',
+          color: '#ffffff',
+          gradient: {
+            type: 'linear',
+            rotation: 0,
+            colorStops: [
+              { offset: 0, color: '#000000' },
+              { offset: 1, color: '#000000' },
+            ],
+          },
+        }"
+          :backgroundOptions="{ color: '#ffffff' }"
+          :cornersSquareOptions="{ type: 'square', color: '#000000' }"
+          :cornersDotOptions="{ type: undefined, color: '#000000' }"
+          fileExt="png"
+          :download="false"
+          myclass="my-qur"
+          imgclass="img-qr"
+          downloadButton="my-button"
+          :downloadOptions="{ name: 'vqr', extension: 'png' }"
+        />
+  </div>
+</template>
+
+<script>
+import QRCodeVue3 from "qrcode-vue3";
+
+export default {
+  name: 'QRCodeVue3Example',
+  components: {
+    QRCodeVue3
+  },
   
-     <!-- <QRCodeVue3
-            value="Simple QR code"
-          /> -->
-  
-     <QRCodeVue3
-            :width="500"
-            :height="500"
-            value="https://836b-213-234-222-188.ngrok-free.app?company=1"
-            :qrOptions="{ typeNumber: 0, mode: 'Byte', errorCorrectionLevel: 'Q' }"
-            :imageOptions="{ hideBackgroundDots: true, imageSize: 0.4, margin: 0 }"
-            :dotsOptions="{
-            type: 'classy',
-            color: '#ffffff',
-            gradient: {
-              type: 'linear',
-              rotation: 0,
-              colorStops: [
-                { offset: 0, color: '#000000' },
-                { offset: 1, color: '#000000' },
-              ],
-            },
-          }"
-            :backgroundOptions="{ color: '#ffffff' }"
-            :cornersSquareOptions="{ type: 'dot', color: '#000000' }"
-            :cornersDotOptions="{ type: undefined, color: '#000000' }"
-            fileExt="png"
-            :download="false"
-            myclass="my-qur"
-            imgclass="img-qr"
-            downloadButton="my-button"
-            :downloadOptions="{ name: 'vqr', extension: 'png' }"
-          />
-    </div>
-  </template>
-  
-  <script>
-  import QRCodeVue3 from "qrcode-vue3";
-  
-  export default {
-    name: 'QRCodeVue3Example',
-    components: {
-      QRCodeVue3
-    },
-    
 //     data: {
 //     company: 'ПАО Южный Кузбасс',
 //     otdel: 'Отдел управления рисками',
@@ -52,8 +52,8 @@
 //     subject: 'Обращение',
 //     body: '-'
 // }
-  }
-  </script>
+}
+</script>
 
 
 <script>
